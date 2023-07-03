e138115ad1bb2cbe9412b1df1f5f216fa8a92553
kirank860 <kkiru889@gmail.com>
2023-07-03 12:44:30 +0530

    first commit

---
+++ Api/api.js
@@ -0,0 +1,15 @@
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => response.json()) // Assuming the API response is in JSON format
+//   .then(data =>  {
+  
+
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log("hai"))
+//     // Update the HTML content with the fetched data
+//     const aboutTextElement = document.querySelector('.about-text p');
+//     aboutTextElement.textContent = data.title;
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
--- js/main.js
+++ js/main.js
@@ -103,4 +103,148 @@
     });
     
 })(jQuery);
+//.....api.......//
+let about='http://localhost:8050/api/v1/about-us'
+let contact='http://localhost:8050/api/v1/contact-us'
+let casestudy="http://localhost:8050/api/v1/our-speciality"
+let practicearea=""
+let socialmedia="'http://localhost:8050/api/v1/social-media'"
+let postmanagment="http://localhost:8050/api/v1/post-management"
+let postcategory="http://localhost:8050/api/v1/post-category"
 
+//    about pAge API
+
+fetch('http://localhost:8050/api/v1/about-us')
+  .then(response => response.json()) // Assuming the API response is in JSON format
+  .then(data =>  {
+    console.log("about api",data.response)
+    // Update the HTML content with the fetched data
+
+//     const aboutTextElement = document.querySelector('.carousel-caption h1');
+//     aboutTextElement.textContent = data.response[0].title;
+
+//     const subTitle = document.querySelector('.carousel-caption h2');
+//     subTitle.textContent = data.response[0].subTitle;
+
+
+//     const para = document.querySelector('.carousel-caption p');
+//     para.textContent = data.response[0].description;
+
+//     const title2=document.querySelector("#scndcarotitle")
+//     title2.textContent=data.response[0].title
+
+//     const para2=document.querySelector("#scndcaropara")
+//     para2.textContent=data.response[0].description
+
+//     const title3=document.querySelector("#scndcaro3title")
+//     title3.textContent=data.response[0].title
+
+//     const para3=document.querySelector("#scndcaro3para")
+//     para3.textContent=data.response[0].description
+
+//     const image = document.querySelector('.about-img img');
+//     image.src = data.response[0].aboutusImage;  
+
+//     const caroimg= document.querySelector("#caro-img-1")
+//     caroimg.src=data.response[0].bannerImage
+
+//     const caroimg_2= document.querySelector("#caro-img-2")
+//     caroimg_2.src=data.response[0].bannerImage
+
+//     const caroimg_3= document.querySelector("#caro-img-3")
+//     caroimg_3.src=data.response[0].bannerImage
+
+//     //  about section AP..........
+//     const ABOUTP1=document.querySelector(".abtp")
+//     ABOUTP1.textContent=data.response[0].description
+    
+//     const ABOUTP2=document.querySelector(".abtp2")
+//     ABOUTP2.textContent=data.response[0].description
+
+//    const aboutimg=document.querySelector(".about-img img")
+//     aboutimg.src=data.response[0].aboutusImage
+
+//     const abouttitle=document.querySelector(".section-header h2")
+//     abouttitle.textContent=data.response[0].title
+
+//     const absubTitle = document.querySelector(".timeline-content h2");
+//     absubTitle.textContent = data.response[0].subTitle;
+
+      
+//     const abtpara=document.querySelector(".timeline-content p" )
+//     abtpara.textContent=data.response[0].description
+
+//     const footerpara=document.querySelector(".footer-about p" )
+//     footerpara.textContent=data.response[0].description
+    
+     const aboutimg=document.querySelector(".about-img img")
+     aboutimg.src=data.response[0].aboutusImage
+
+     const ABOUTP1=document.querySelector(".abp1")
+     ABOUTP1.textContent=data.response[0].description
+
+     const ABOUTP2=document.querySelector(".abp2")
+     ABOUTP2.textContent=data.response[0].description
+
+
+     const ABOUTfP1=document.querySelector(".footer-about p")
+     ABOUTfP1.textContent=data.response[0].description
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+//   CONTACT PAGE API......
+fetch('http://localhost:8050/api/v1/contact-us')
+.then(response => response.json()) // Assuming the API response is in JSON format
+.then(contact=>  {
+  console.log( "contact api",contact.response)
+  // Update the HTML content with the fetched data
+  const location=document.querySelector(".location" )
+  location.textContent=contact.response[0].locationUrl
+
+  const phone=document.querySelector(".phnumber" )
+  phone.textContent=contact.response[0].primaryPhone
+
+
+  const email=document.querySelector(".email" )
+  email.textContent=contact.response[0].primaryEmail
+
+})
+.catch(error => {
+  console.error('Error:', error);
+});
+
+
+// case study........
+fetch('http://localhost:8050/api/v1/our-speciality')
+.then(response => response.json()) // Assuming the API response is in JSON format
+.then(contact=>  {
+  console.log("speciality api",contact.response)
+//   Update the HTML content with the fetched data
+  const cimg1=document.querySelector(".cimg1" )
+  cimg1.src=contact.response[0].specialityImage
+
+
+  const cimg2=document.querySelector(".cimg2" )
+  cimg2.src=contact.response[0].specialityImage
+
+  const cimg3=document.querySelector(".cimg3" )
+  cimg3.src=contact.response[0].specialityImage
+
+
+  const cimg4=document.querySelector(".cimg4" )
+  cimg4.src=contact.response[0].specialityImage
+
+  const cimg5=document.querySelector(".cimg5" )
+  cimg5.src=contact.response[0].specialityImage
+
+  const cimg6=document.querySelector(".cimg6" )
+  cimg6.src=contact.response[0].specialityImage
+
+})
+
+.catch(error => {
+  console.error('Error:', error);
+});
